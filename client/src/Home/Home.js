19396aa6e4d6355569f2015fcc44450b0f802141
Devina <devinardya@gmail.com>
2020-05-27 13:28:43 +0200

    fixed page title change

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logoicon from '../Image/toodo-logo2.svg';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import {Link} from 'react-router-dom';
 import '../index.scss';
 
 const Home = () => {
+
+    const [home, updateHome] = useState("");
+
+    useEffect( () => {
+        updateHome("Welcome to Toodo");
+    }, [])
     
     return <HelmetProvider>
                 <Helmet>
-                    <title>Welcome to Toodo</title>
+                    <title>{home}</title>
                 </Helmet>
                 <div className="home-block">
                     <figure className="home-block-logo">
