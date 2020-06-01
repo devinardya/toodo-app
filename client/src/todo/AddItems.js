7c1d8eeb27601f0d2f04df0ddea09490a6f72b50
Devina <devinardya@gmail.com>
2020-06-01 11:50:14 +0200

    cleaning code

---
+++ client/src/todo/AddItems.js
@@ -0,0 +1,76 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+import {IoIosAddCircle, IoMdClose} from 'react-icons/io';
+
+const AddItems = ({
+    todobox,
+    todo,
+    userName,
+    updateTodobox,
+}) => {
+
+    const [addListFormActive, updateAddListFormActive] = useState(false);
+    const [addListInput, updateAddListText] = useState("");
+
+    const onAddListChange = (e) => {
+        let data = e.target.value;
+        updateAddListText(data);
+    };
+
+    const addListActive = () => {
+        updateAddListFormActive(true);
+    };
+
+    const addNewList = (e, id) => {
+
+        e.preventDefault();
+
+
+        if(addListInput.length !== 0){
+
+            let input = {
+                todoTitle : addListInput,
+                description : "",
+            }
+            axios.post("/todos/" + id + "/user/" + userName, input)
+            .then(response => {   
+                console.log("response after adding data", response.data);
+                let copyData = [...todobox];
+                let index = copyData.findIndex(x => x._id === id);
+                copyData[index].data = [...copyData[index].data, response.data];
+                updateTodobox(copyData);
+                updateAddListFormActive(false);
+            })
+            .catch( err => {
+                console.log(err);
+            })
+
+            updateAddListText("");
+        } 
+    };
+    
+    const cancel = () => {
+        updateAddListFormActive(false);
+    };
+
+    return <div className="block__board--main--eachbox--addItem">
+                {addListFormActive ? 
+                    <form className="block__board--main--eachbox--addItem__addform" onSubmit = {(e) => addNewList(e, todo._id)} >
+                        <div className="block__board--main--eachbox--addItem__addform--inputbox">
+                            <input onChange={onAddListChange} placeholder="Enter title for this item..." type="text" value={addListInput}/>
+                        </div>
+                        <div className="block__board--main--eachbox--addItem__addform--formbutton">
+                            <button className="block__board--main--eachbox--addItem__addform--formbutton--addList">Add Item</button>
+                            <div className="block__board--main--eachbox--addItem__addform--formbutton--cancel" onClick={cancel}><IoMdClose /></div>
+                        </div>
+                    </form>
+                    : 
+                    <button className="block__board--main-addButton" onClick={() => addListActive()}>
+                        <IoIosAddCircle style={{position:"relative", top: "2px", marginRight:"10px"}}/>
+                        Add new item
+                    </button>
+                }
+            </div>      
+};
+
+export default AddItems;
--- client/src/todo/TodoBox.js
+++ client/src/todo/TodoBox.js
@@ -1,8 +1,7 @@
-import React, {useState} from 'react';
-import {IoIosAddCircle, IoMdClose} from 'react-icons/io';
-import axios from 'axios';
+import React from 'react';
 import ListBox from './ListBox';
 import Dropdown from './Dropdown';
+import AddItems from './AddItems';
 
 const TodoBox = ({
     todobox,
@@ -10,50 +9,6 @@ const TodoBox = ({
     userName,
     updateTodobox,
 }) => {
-
-    const [addListFormActive, updateAddListFormActive] = useState(false);
-    const [addListInput, updateAddListText] = useState("");
-
-    const onAddListChange = (e) => {
-        let data = e.target.value;
-        updateAddListText(data);
-    };
-
-    const addListActive = () => {
-        updateAddListFormActive(true);
-    };
-
-    const addNewList = (e, id) => {
-
-        e.preventDefault();
-
-
-        if(addListInput.length !== 0){
-
-            let input = {
-                todoTitle : addListInput,
-                description : "",
-            }
-            axios.post("/todos/" + id + "/user/" + userName, input)
-            .then(response => {   
-                console.log("response after adding data", response.data);
-                let copyData = [...todobox];
-                let index = copyData.findIndex(x => x._id === id);
-                copyData[index].data = [...copyData[index].data, response.data];
-                updateTodobox(copyData);
-                updateAddListFormActive(false);
-            })
-            .catch( err => {
-                console.log(err);
-            })
-
-            updateAddListText("");
-        } 
-    };
-    
-    const cancel = () => {
-        updateAddListFormActive(false);
-    };
 
     return <div className="block__board--main--eachbox">
                 <div className="block__board--main--eachbox--title" >
@@ -81,24 +36,12 @@ const TodoBox = ({
                     }
                     </ul>
                 </span>
-                <div className="block__board--main--eachbox--addItem">
-                    {addListFormActive ? 
-                        <form className="block__board--main--eachbox--addItem__addform" onSubmit = {(e) => addNewList(e, todo._id)} >
-                            <div className="block__board--main--eachbox--addItem__addform--inputbox">
-                                <input onChange={onAddListChange} placeholder="Enter title for this item..." type="text" value={addListInput}/>
-                            </div>
-                            <div className="block__board--main--eachbox--addItem__addform--formbutton">
-                                <button className="block__board--main--eachbox--addItem__addform--formbutton--addList">Add Item</button>
-                                <div className="block__board--main--eachbox--addItem__addform--formbutton--cancel" onClick={cancel}><IoMdClose /></div>
-                            </div>
-                        </form>
-                        : 
-                        <button className="block__board--main-addButton" onClick={() => addListActive()}>
-                            <IoIosAddCircle style={{position:"relative", top: "2px", marginRight:"10px"}}/>
-                            Add new item
-                        </button>
-                    }
-                </div>
+                <AddItems 
+                    todobox = {todobox}
+                    todo = {todo}
+                    userName = {userName}
+                    updateTodobox = {updateTodobox}
+                />
             </div>         
 };
 
